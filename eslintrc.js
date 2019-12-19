2a81bb7e59f88d477505552d86c40d31de1bbe54
Jacobo Martínez <35739742+cobidev@users.noreply.github.com>
2019-12-19 14:22:00 -0300

    Create eslintrc.js

---
+++ eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ['airbnb', 'prettier'],
+  plugins: ['prettier'],
+  env: {
+    browser: true,
+    es6: true,
+  },
+  rules: {
+    'import/no-extraneous-dependencies': 'off',
+    'prettier/prettier': 'error',
+    'no-console': 'warn',
+  },
+};
