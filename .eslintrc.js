b11b379870719f313ddb54ad6ebad8cd971269bf
Jacobo Martínez <35739742+cobidev@users.noreply.github.com>
2020-01-06 19:08:12 -0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,19 @@
 module.exports = {
-  extends: ['airbnb', 'prettier'],
-  plugins: ['prettier'],
+  extends: ["airbnb", "prettier"],
+  plugins: ["prettier"],
   env: {
     browser: true,
     es6: true,
   },
   rules: {
-    'import/no-extraneous-dependencies': 'off',
-    'prettier/prettier': 'error',
-    'no-console': 'warn',
-    'react/jsx-one-expression-per-line': 'off',
-    'react/prefer-stateless-function': 'off', // disable if necessary
-    'react/jsx-filename-extension': 'off', // disable if necessary
-    'react/static-property-placement': 'off', // disable if necessary
+    "import/no-extraneous-dependencies": "off",
+    "prettier/prettier": "error",
+    "no-console": "warn",
+    "react/jsx-one-expression-per-line": "off",
+    "react/jsx-filename-extension": "off", // disable if necessary
+    "react/static-property-placement": "off", // disable if necessary
+    "react/jsx-props-no-spreading": "off", // disable if necessary
+    "react/require-default-props": "off",
   },
-  parser: 'babel-eslint',
-};
+  parser: "babel-eslint",
+}
